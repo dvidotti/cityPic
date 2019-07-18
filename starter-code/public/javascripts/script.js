7f5d3cfcb06865fb282c4a76b62ef66f7639539c
dvidotti <danilovidotti@gmail>
2019-07-17 21:53:55 -0300

    firstDeploy

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,132 @@
-document.addEventListener('DOMContentLoaded', () => {
+  const ironhackSP = {
+    lat: -23.5617375,
+    lng: -46.6601331
+  };
+  
+  const markers = []
+  
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: ironhackSP
+  });
 
-  console.log('IronGenerator JS imported successfully!');
+  let center = {
+    lat: undefined,
+    lng: undefined
+  }; 
 
-}, false);
+
+function getPlaces() {
+  axios.get("/api")
+   .then(response => {
+     console.log(response.data);
+     markPlaces(response.data.places);
+
+   })
+   .catch(error => {
+     console.log(error);
+   })
+ }
+
+
+ function markPlaces(places){
+  places.forEach((place) => {
+    const center = {
+      lat: place.location.coordinates[1],
+      lng: place.location.coordinates[0]
+    };
+    const pin = new google.maps.Marker({
+      position: center,
+      map: map,
+      title: place.name
+    });
+    console.log(pin)
+    markers.push(pin);
+  });
+}
+
+getPlaces();
+
+
+  // if (navigator.geolocation) {
+
+  //   // Get current position
+  //   // The permissions dialog will pop up
+  //   navigator.geolocation.getCurrentPosition(function (position) {
+  //     // Create an object to match Google's Lat-Lng object format
+  //     const center = {
+  //       lat: position.coords.latitude,
+  //       lng: position.coords.longitude
+  //     };
+  //     const map = new google.maps.Map(
+  //         document.getElementById('map'),
+  //         {
+  //           zoom: 15,
+  //           center: center,
+  //         }
+  //       );
+
+  //       const myLoc = new google.maps.Marker({
+  //         position: {
+  //           lat: position.coords.latitude,
+  //           lng: position.coords.longitude
+  //         },
+  //         map: map,
+  //         title: "MyLoc"
+  //       });
+      
+
+  //       const ironhack = new google.maps.Marker({
+  //         position: {
+  //           lat: -23.560636,
+  //           lng: -46.660262
+  //         },
+  //         map: map,
+  //         title: "Work"
+  //       });
+
+  //       const casa = new google.maps.Marker({
+  //         position: {
+  //           lat: -23.578067,
+  //           lng:  -46.633585
+  //         },
+  //         map: map,
+  //         title: "Casa"
+  //       });
+      
+
+  //     console.log('center: ', center)
+  //     // User granted permission
+  //     // Center the map in the position we got
+  //   }, function () {
+  //     // If something goes wrong
+  //     console.log('Error in the geolocation service.');
+  //   });
+  // } else {
+  //   // Browser says: Nah! I do not support this.
+  //   console.log('Browser does not support geolocation.');
+  // }
+
+
+
+
+// ------------------------------------------------------------ //
+
+// window.onload = () => {
+//   const ironhackBCN = {
+//     lat: 41.386230, 
+//     lng: 2.174980
+//   };
+  
+//   // const markers = []
+  
+//   const map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 13,
+//     center: ironhackBCN
+//   });
+
+//   // let center = {
+//   //   lat: undefined,
+//   //   lng: undefined
+//   // }; 
+// };
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,77 @@
 const express = require('express');
 const router  = express.Router();
+const mongoose     = require('mongoose');
+const Place = require('../models/Places')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/api', (req, res, next) => {
+  Place.find()
+    .then(places => {
+      res.status(200).json({ places });
+    })
+    .catch(error => console.log(error))
+});
+
+router.post('/add-place', (req, res, next) => {
+  const {name, type} = req.body;
+  let location = {
+    type: 'Point',
+    coordinates: [req.body.longitude, req.body.latitude]
+    };
+  if(name === '' || type === '' || location ==='') {
+    res.render('/index', {error: 'Fill the form'})
+    return;
+  }
+  Place.findOne({name})
+  .then(place => {
+    if (place !== null) {
+      res.render('index', {error: 'The place already exists'});
+      return;
+    }
+  Place.create({name, type, location})
+  .then(place => {
+    res.render('index', {message: `${place.name} saved`})
+  })
+  })
+})
+
+router.get('/places', (req, res, next) => {
+  Place.find()
+  .then(place => {
+    res.render('places', { place })
+  })
+})
+
+router.get('/delete/:id', (req, res, next) => {
+  const placeId = req.params.id;
+  Place.findByIdAndDelete(placeId)
+  .then(place => {
+    res.redirect('/places')
+  })
+})
+
+router.get('/edit/:id', (req, res, next) => {
+  const placeId = req.params.id;
+  Place.findById(placeId)
+  .then(place => {
+    console.log(place.location.coordinates);
+    res.render('edit-place', place)
+  })
+})
+
+router.post('/edit/:place', (req, res, next) => {
+  const id = req.params.place;
+  const { name, type } = req.body;
+  Place.findByIdAndUpdate(id, {name: name, type: type})
+  .then(place => {
+    res.redirect('/places')
+  })
+})
+
+
 
 module.exports = router;
